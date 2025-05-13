f744516af2afaafef2dc937556f7f2b2c9a97de5
Venkatesh Pise <venkateshpise97@gmail.com>
2025-05-13 18:37:22 +0530

    screen size issue solved

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,10 @@ document.addEventListener("DOMContentLoaded", function() {
         window.location.href = "play.html"; // Navigate to game screen
     });
 });
+
+function setVhUnit() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+setVhUnit();
+window.addEventListener("resize", setVhUnit);
--- assets/js/resultscreen.js
+++ assets/js/resultscreen.js
@@ -22,3 +22,10 @@ window.onload = function() {
 $("#replay").click(function() {
     window.location.href = "play.html";
 });
+
+function setVhUnit() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+setVhUnit();
+window.addEventListener("resize", setVhUnit);
